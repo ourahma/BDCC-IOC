4b2291bda5d0af1be63f235e8810e24a22d17b15
ourahmamaroua <122113141+ourahma@users.noreply.github.com>
2025-04-12 11:32:25 +0000

    Instantiation automatique de IMetier et appeler le setter setDao pour faire l'injection

--- src/main/java/net/ourahma/pres/pres2.java
+++ src/main/java/net/ourahma/pres/pres2.java
@@ -1,12 +1,16 @@
 package net.ourahma.pres;
 
 import net.ourahma.dao.IDao;
+import net.ourahma.metier.IMetier;
 
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
 import java.util.Scanner;
 
 public class pres2 {
+    // FileNotFoundException, ClassNotFoundException, InstantiationException, IllegalAccessException, NoSuchMethodException, InvocationTargetException 
     public static void main(String[] args) throws Exception{
         // lire le fichier
         Scanner scanner = new Scanner(new File("config.txt"));
@@ -20,6 +24,15 @@ public class pres2 {
         Class cDao = Class.forName(docClassName);
         // créer l'objet
         IDao dao = (IDao) cDao.newInstance();
-        System.out.println(dao.getData());
+        //System.out.println(dao.getData());
+
+        // faire la meme chose pour l'objet matier
+        String metierClassName = scanner.nextLine();
+        Class cmetier = Class.forName(metierClassName);
+        //IMetier metier = (IMetier) cmetier.getConstructor(IDao.class).newInstance(dao);
+        IMetier metier = (IMetier) cmetier.getConstructor().newInstance();
+        Method setDao = cmetier.getMethod("setDao", IDao.class);
+        setDao.invoke(metier, dao);
+        System.out.println("RES = " + metier.calculer());
     }
 }
